b02b44d1da7e9ddd7a341d29a597accfbb78155c
polo <ordipolo@gmx.fr>
2025-08-22 15:19:06 +0200

    Amélioration de copy_directory.php

--- bin/copy_directory.php
+++ bin/copy_directory.php
@@ -1,11 +1,18 @@
 <?php
 // bin/copy_directory.php
 //
-// appel dans le composer.json
+// usage : php bin/copy_directory.php source destination
+// appel dans le composer.json à l'intallation et lors des MAJ
 
-function copyDirectory($source, $destination) {
-    if (!is_dir($destination)) {
-        mkdir($destination, 0777, true);
+function copyDirectory(string $source, string $destination): void
+{
+    if (!is_dir($source)) {
+        fwrite(STDERR, "Erreur rencontrée dans bin/copy_directory.php: le dossier source '$source' n'est pas un répertoire valide.\n");
+        exit(1);
+    }
+
+    if(!is_dir($destination)){
+        mkdir($destination, 0755, true);
     }
 
     $iterator = new RecursiveIteratorIterator(
@@ -13,13 +20,21 @@ function copyDirectory($source, $destination) {
         RecursiveIteratorIterator::SELF_FIRST
     );
 
-    foreach ($iterator as $item) {
-        if ($item->isDir()) {
-            mkdir($destination . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
-        } else {
-            copy($item, $destination . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+    foreach($iterator as $item){
+        $targetPath = $destination . DIRECTORY_SEPARATOR . $iterator->getSubPathName();
+        if($item->isDir()){
+            if(!is_dir($targetPath)){
+                mkdir($targetPath, 0755);
+            }
+        }
+        else{
+            copy($item, $targetPath); // copy() écrase la cible, c'est ce qu'on veut
         }
     }
 }
 
+if ($argc != 3){ // nombre de paramètres
+    fwrite(STDERR, "Erreur rencontrée dans bin/copy_directory.php. Usage:\nphp bin/copy_directory.php <source> <destination>\n");
+    exit(1);
+}
 copyDirectory($argv[1], $argv[2]);
