fc14268ad5d644680a5802eb1af4691bb9294dd5
polo <ordipolo@gmx.fr>
2025-12-17 12:21:34 +0100

    bug chemin exécution de cron.php

--- bin/cron.php
+++ bin/cron.php
@@ -4,7 +4,7 @@
 
 declare(strict_types=1);
 
-chdir('bin');
+chdir(dirname(__FILE__)); // = /chemin/du/site/bin = même niveau que public/
 require('../src/Config.php');
 Config::load('../config/config.ini');
 require '../src/model/doctrine-bootstrap.php';
